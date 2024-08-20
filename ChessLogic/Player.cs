fa07c213b1cda14b2b4c1b99bbad5ca8bbd9eb29
MrRobot <juaniquinapokar@gmail.com>
2024-08-20 08:51:16 -0400

    player Opponent

--- ChessLogic/Player.cs
+++ ChessLogic/Player.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChessLogic
+﻿namespace ChessLogic
 {
     public enum Player
     {
         None,
         White,
         Black
+    }
+    public static class PlayerExtensions
+    {
+        public static Player Opponent(this Player player)
+        {
+            return player switch
+            {
+                Player.White => Player.Black,
+                Player.Black => Player.White,
+                _ => Player.None,
+            };
+        }
     }
 }
