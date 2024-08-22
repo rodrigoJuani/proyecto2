629b86c80f306ad381184abef1829dd7e4bf65ea
MrRobot <juaniquinapokar@gmail.com>
2024-08-22 12:55:51 -0400

    direction class

---
+++ ChessLogic/Direction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public class Direction
+    {
+        public int RowDelta { get; }
+        public int ColumnDelta { get; }
+        public Direction(int rowDelta, int columnDelta)
+        {
+            RowDelta = rowDelta;
+            ColumnDelta = columnDelta;
+        }
+    }
+}
