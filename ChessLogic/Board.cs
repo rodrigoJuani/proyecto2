281cde7af942f95d716e0a27e7c457c16ab3b713
MrRobot <juaniquinapokar@gmail.com>
2024-08-30 17:58:32 -0400

    Piezas blancas

--- ChessLogic/Board.cs
+++ ChessLogic/Board.cs
@@ -20,5 +20,22 @@ namespace ChessLogic
             get { return this[pos.Row, pos.Column]; }
             set { this[pos.Row, pos.Column] = value; }
         }
+        public static Board Initial()
+        {
+            Board board = new Board();
+            return board;
+        }
+        private void AddStartPieces()
+        {
+            this[0, 0] = new Rook(Player.Black);
+            this[0, 1] = new Knight(Player.Black);
+            this[0, 2] = new Bishop(Player.Black);
+            this[0, 3] = new Queen(Player.Black);
+            this[0, 4] = new King(Player.Black);
+            this[0, 5] = new Bishop(Player.Black);
+            this[0, 6] = new Knight(Player.Black);
+            this[0, 7] = new Rook(Player.Black);
+
+        }
     }
 }
