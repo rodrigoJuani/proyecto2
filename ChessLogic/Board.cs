37d4cf043dde7bfe2b8a852ec5b6f118420d928b
MrRobot <juaniquinapokar@gmail.com>
2024-08-31 14:13:33 -0400

    isinside y isempty

--- ChessLogic/Board.cs
+++ ChessLogic/Board.cs
@@ -50,7 +50,14 @@ namespace ChessLogic
                 this[6, c] = new Pawn(Player.White);
 
             }
-
+        }
+        public static bool IsInside(Position pos)
+        {
+            return pos.Row >= 0 && pos.Row < 8 && pos.Column >= 0 && pos.Column < 8;
+        }
+        public bool IsEmpty(Position pos)
+        {
+            return this[pos] == null;
         }
     }
 }
