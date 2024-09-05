548276fcae7260025db071dd4c9e745299ecee39
MrRobot <juaniquinapokar@gmail.com>
2024-09-05 11:08:22 -0400

    piezas blancas en la nueva clase

--- ChessUI/Images.cs
+++ ChessUI/Images.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using ChessLogic;
 namespace ChessUI
 {
-    class Images
+    public static class Images
     {
+        private static readonly Dictionary<PieceType, ImageSource> whiteSources = new()
+        {
+            {PieceType.Pawn, LoadImage("Assets/PawnW.png") },
+            {PieceType.Bishop, LoadImage("Assets/Bishow.png") },
+            {PieceType.knight, LoadImage("Assets/knightW.png") },
+            {PieceType.Rook, LoadImage("Assets/RookW.png") },
+            {PieceType.Queen, LoadImage("Assets/QueenW.png") },
+            {PieceType.King, LoadImage("Assets/KingW.png") }
+        }
+        private static ImageSource LoadImage(string filePath)
+        {
+            return new BitmapImage(new Uri(filePath, UriKind.Relative));
+        }
     }
 }
