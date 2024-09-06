45cc0c5a924b438336e29518f45538c544c49110
MrRobot <juaniquinapokar@gmail.com>
2024-09-06 18:04:20 -0400

    loading piece images

--- ChessUI/Images.cs
+++ ChessUI/Images.cs
@@ -13,12 +13,21 @@ namespace ChessUI
         private static readonly Dictionary<PieceType, ImageSource> whiteSources = new()
         {
             {PieceType.Pawn, LoadImage("Assets/PawnW.png") },
-            {PieceType.Bishop, LoadImage("Assets/Bishow.png") },
+            {PieceType.Bishop, LoadImage("Assets/BishoW.png") },
             {PieceType.knight, LoadImage("Assets/knightW.png") },
             {PieceType.Rook, LoadImage("Assets/RookW.png") },
             {PieceType.Queen, LoadImage("Assets/QueenW.png") },
-            {PieceType.King, LoadImage("Assets/KingW.png") }
-        }
+            {PieceType.king, LoadImage("Assets/KingW.png") }
+        };
+        private static readonly Dictionary<PieceType, ImageSource> blackSources = new()
+        {
+            {PieceType.Pawn, LoadImage("Assets/PawnB.png") },
+            {PieceType.Bishop, LoadImage("Assets/BishoB.png") },
+            {PieceType.knight, LoadImage("Assets/knightB.png") },
+            {PieceType.Rook, LoadImage("Assets/RookB.png") },
+            {PieceType.Queen, LoadImage("Assets/QueenB.png") },
+            {PieceType.king, LoadImage("Assets/KingB.png") }
+        };
         private static ImageSource LoadImage(string filePath)
         {
             return new BitmapImage(new Uri(filePath, UriKind.Relative));
