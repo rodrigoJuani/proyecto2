2122cc584b519545b5d8d6f52ed40a6d7608c5c6
MrRobot <juaniquinapokar@gmail.com>
2024-09-06 18:11:43 -0400

    getimage method

--- ChessUI/Images.cs
+++ ChessUI/Images.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using ChessLogic;
@@ -31,6 +28,23 @@ namespace ChessUI
         private static ImageSource LoadImage(string filePath)
         {
             return new BitmapImage(new Uri(filePath, UriKind.Relative));
+        }
+        public static ImageSource GetImage(Player color, PieceType type)
+        {
+            return color switch
+            {
+                Player.White => whiteSources[type],
+                Player.Black => blackSources[type],
+                _ =>null
+            };
+        }
+        public static ImageSource GetImage(Piece piece)
+        {
+            if (piece == null)
+            {
+                return null;
+            }
+            return GetImage(piece.Color, piece.Type);
         }
     }
 }
