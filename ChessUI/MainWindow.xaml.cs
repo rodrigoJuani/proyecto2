1e238e6674a8df46297fe394c49593c6796bb328
MrRobot <juaniquinapokar@gmail.com>
2024-09-07 13:45:20 -0400

    draw chess pieces

--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -18,10 +18,14 @@ namespace ChessUI
     public partial class MainWindow : Window
     {
         private readonly Image[,] pieceImages = new Image[8, 8];
+        private GameState gameState;
         public MainWindow()
         {
             InitializeComponent();//este es un comentario
             InitializeBoard();
+
+            gameState = new GameState(Player.White, Board.Initial());
+            DrawBoard(gameState.Board);
         }
         private void InitializeBoard()
         {
@@ -32,6 +36,17 @@ namespace ChessUI
                     Image image = new Image();
                     pieceImages[r, c] = image;
                     PieceGrid.Children.Add(image);
+                }
+            }
+        }
+        private void DrawBoard(Board board)
+        {
+            for(int r = 0; r < 8; r++)
+            {
+                for(int c = 0; c < 8; c++)
+                {
+                    Piece piece = board[r, c];
+                    pieceImages[r, c].Source = Image.GetImage(piece);
                 }
             }
         }
