c686a22ec98e7c504bfef69fa3189c11653f60f1
MrRobot <juaniquinapokar@gmail.com>
2024-09-11 12:16:18 -0400

    NormalMove implementacion

---
+++ ChessLogic/Moves/NormalMove.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public class NormalMove:Move
+    {
+        public override MoveType Type => MoveType.Normal;
+        public override Position FromPos { get; }
+        public override Position ToPos  { get;}
+        public NormalMove (Position from,Position to)
+     {
+        FromPos = from;
+        ToPos = to;
+     }
+    }
+}
