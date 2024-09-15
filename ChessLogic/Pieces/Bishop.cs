b29c10a601e4f201902dc266c2a3103d027a9038
MrRobot <juaniquinapokar@gmail.com>
2024-09-15 11:45:00 -0400

    bishop moves

--- ChessLogic/Pieces/Bishop.cs
+++ ChessLogic/Pieces/Bishop.cs
@@ -10,6 +10,13 @@ namespace ChessLogic
     {
         public override PieceType Type => PieceType.Bishop;
         public override Player Color { get; }
+        private static readonly Direction[] dirs = new Direction[]
+        {
+            Direction.NorthWest,
+            Direction.NorthEast,
+            Direction.SouthWest,
+            Direction.SouthEast
+        };
         public Bishop (Player color)
         {
             Color = color;
@@ -19,6 +26,10 @@ namespace ChessLogic
             Bishop copy=new Bishop(Color );
             copy.HasMoved = HasMoved;
             return copy;
+        }
+        public override IEnumerable<Move>GetMoves(Position from,Board board)
+        {
+            return MovePositionsInDirs(from, board, dirs).Select(to => new NormalMove(from, to));
         }
     }
 }
--- ChessLogic/Pieces/Piece.cs
+++ ChessLogic/Pieces/Piece.cs
@@ -31,5 +31,9 @@ namespace ChessLogic
                 yield break;
             }
         }
+        protected IEnumerable<Position> MovePositionsInDirs(Position from,Board board, Direction[] dirs)
+        {
+            return dirs.SelectMany(dir => MovePositionsInDir(from, board, dir));
+        }
     }
 }
