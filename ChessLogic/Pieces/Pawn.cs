1f7bc7308402759f2c47b929a3a2b30cc4868495
MrRobot <juaniquinapokar@gmail.com>
2024-09-17 09:08:21 -0400

    pawn moves 1

--- ChessLogic/Pieces/Pawn.cs
+++ ChessLogic/Pieces/Pawn.cs
@@ -10,15 +10,34 @@ namespace ChessLogic
     {
         public override PieceType Type=>PieceType.Pawn;
         public override Player Color { get; }
+        private readonly Direction forward;
         public Pawn(Player color)
         {
             Color = color;
+            if (color == Player.White)
+            {
+                forward = Direction.North;
+            }else if(color==Player.Black){
+                forward = Direction.South;
+            }
         }
         public override Piece Copy()
         {
             Pawn copy = new Pawn(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+        private static bool CanMoveTo(Position pos,Board board)
+        {
+            return Board.IsInside(pos) && board.IsEmpty(pos);
+        }
+        private bool CanCaptureAt(Position pos,Board board)
+        {
+            if(!Board.IsInside(pos) || board.IsEmpty(pos))
+            {
+                return false;
+            }
+            return board[pos].Color != Color;
         }
     }
 }
