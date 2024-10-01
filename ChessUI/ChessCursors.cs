f41cb43644014de3aa3537eb19698085a11d2af4
MrRobot <juaniquinapokar@gmail.com>
2024-10-01 17:32:00 -0400

    LoaderOptimization cursors

--- ChessUI/ChessCursors.cs
+++ ChessUI/ChessCursors.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace ChessUI
 {
     public static class ChessCursors
     {
-        
+        public static readonly Cursor WhiteCursor = LoadCursor("Assets/CursorW.cur");
+        public static readonly Cursor BlackCursor = LoadCursor("Assets/CursorB.cur");
+        private static Cursor LoadCursor(String filePath)
+        {
+            Stream stream = Application.GetResourceStream(new Uri(filePath, Urikind.Relative)).Stream;
+            return new Cursor(stream, true);
+       
     }
 }
