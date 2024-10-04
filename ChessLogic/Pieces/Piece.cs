9cfb561bae861838d7af51fc15f794ea9b0b7de6
MrRobot <juaniquinapokar@gmail.com>
2024-10-04 17:28:59 -0400

    override for pawn

--- ChessLogic/Pieces/Piece.cs
+++ ChessLogic/Pieces/Piece.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChessLogic
+﻿namespace ChessLogic
 {
     public abstract class Piece
     {
@@ -34,6 +28,14 @@ namespace ChessLogic
         protected IEnumerable<Position> MovePositionsInDirs(Position from,Board board, Direction[] dirs)
         {
             return dirs.SelectMany(dir => MovePositionsInDir(from, board, dir));
+        }
+        public virtual bool CanCaptureOpponentKing(Position from,Board board)
+        {
+            return GetMoves(from, board).Any(move =>
+            {
+                Piece piece = board[move.ToPos];
+                return piece != null && piece.Type == PieceType.King;
+            });
         }
     }
 }
--- ChessLogic/Pieces/Queen.cs
+++ ChessLogic/Pieces/Queen.cs
@@ -35,13 +35,5 @@ namespace ChessLogic
         {
             return MovePositionsInDirs(from,board,dirs).Select(to=>new NormalMove(from,to));
         }
-        public virtual bool CanCaptureOpponentKing(Position from,Board board)
-        {
-            return GetMoves(from, board).Any(move =>
-            {
-                Piece piece = board[move.ToPos];
-                return piece != null && piece.Type == PieceType.King;
-            });
-        }
     }
 }
