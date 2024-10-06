6a0838417bd3749fb9c759016701db72aef28918
MrRobot <juaniquinapokar@gmail.com>
2024-10-06 15:41:24 -0400

    Legal move algorithm

--- ChessLogic/Moves/Move.cs
+++ ChessLogic/Moves/Move.cs
@@ -12,5 +12,13 @@ namespace ChessLogic
         public abstract Position FromPos { get; }
         public abstract Position ToPos { get; }
         public abstract void Execute(Board board);
+
+        public virtual bool IsLegal(Board board)
+        {
+            Player player = board[FromPos].Color;
+            Board boardCopy = board.Copy();
+            Execute(boardCopy);
+            return !boardCopy.IsInCheck(player);
+        }
     }
 }
