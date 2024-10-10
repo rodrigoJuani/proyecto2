ec0244586b2db23061be93529683663fdb8111df
MrRobot <juaniquinapokar@gmail.com>
2024-10-10 12:46:52 -0400

    detect checkmate y stalemate

--- ChessLogic/GameState.cs
+++ ChessLogic/GameState.cs
@@ -29,6 +29,34 @@ namespace ChessLogic
         {
             move.Execute(Board);
             CurrentPlayer = CurrentPlayer.Opponent();
+            CheckForGameOver();
+        }
+        public IEnumerable<Move> AllLegalMovesFor(Player player)
+        {
+            IEnumerable<Move> moveCandidates = Board.PiecePositionsFor(player).SelectMany(pos =>
+            {
+                Piece piece = Board[pos];
+                return piece.GetMoves(pos, Board);
+            });
+            return moveCandidates.Where(move => move.IsLegal(Board));
+        }
+        private void CheckForGameOver()
+        {
+            if (!AllLegalMovesFor(CurrentPlayer).Any())
+            {
+                if (Board.IsInCheck(CurrentPlayer))
+                {
+                    Result = Result.Win(CurrentPlayer.Opponent());
+                }
+                else
+                {
+                    Result = Result.Draw(EndReason.Stalemate);
+                }
+            }
+        }
+        public bool IsGameOver()
+        {
+           
         }
     }
 }
