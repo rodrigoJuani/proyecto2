0e471ab55928850d429556bb55fe1cd0d761de97
MrRobot <juaniquinapokar@gmail.com>
2024-10-22 19:55:07 -0400

    pause menu desing 0

--- ChessLogic/GameState.cs
+++ ChessLogic/GameState.cs
@@ -27,6 +27,7 @@ namespace ChessLogic
         }
         public void MakeMove(Move move)
         {
+            Board.SetPawnSkipPosition(CurrentPlayer, null);
             move.Execute(Board);
             CurrentPlayer = CurrentPlayer.Opponent();
             CheckForGameOver();
--- ChessLogic/Pieces/Pawn.cs
+++ ChessLogic/Pieces/Pawn.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ChessLogic
+﻿namespace ChessLogic
 {
     public class Pawn:Piece
     {
@@ -67,7 +61,7 @@ namespace ChessLogic
                 Position twoMovesPos = oneMovesPos + forward;
                 if(!HasMoved && CanMoveTo(twoMovesPos, board))
                 {
-                    yield return new NormalMove(from, twoMovesPos);
+                    yield return new DoublePawn(from, twoMovesPos);
                 }
             }
         }
@@ -76,7 +70,11 @@ namespace ChessLogic
             foreach(Direction dir in new Direction[] { Direction.West, Direction.East })
             {
                 Position to = from + forward + dir;
-                if (CanCaptureAt(to, board))
+
+                if (to == board.GetPawnSkipPosition(Color.Opponent()))
+                {
+                    yield return new EnPassant(from, to);
+                }else  if (CanCaptureAt(to, board))
                 {
                     if (to.Row == 0 || to.Row == 7)
                     {
