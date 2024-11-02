1905995582c596ff8a60eedd728546d7ef45608f
MrRobot <juaniquinapokar@gmail.com>
2024-11-02 14:55:17 -0400

    encode passant17

--- ChessLogic/StateString.cs
+++ ChessLogic/StateString.cs
@@ -17,7 +17,7 @@ namespace ChessLogic
             sb.Append(' ');
             AddCastlingRights(board);
             sb.Append(' ');
-            sb.Append()
+            sb.Append(board,);
         }
         private static char PieceChar(Piece piece)
         {
